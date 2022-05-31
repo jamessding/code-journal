d650e6808cbedde21b6d2a0ebd93bb064be70684
James Ding <jamesding528@yahoo.com>
2022-05-31 14:52:01 +0000

    listen for submit events and prepend new object to data entry

--- js/main.js
+++ js/main.js
@@ -4,5 +4,21 @@ function updatePhoto(event) {
   var url = event.target.value;
   $entryImage.setAttribute('src', url);
 }
-
 $photoUrlInput.addEventListener('input', updatePhoto);
+
+var $saveButton = document.querySelector('.save-button');
+var $form = document.querySelector('.form');
+function clickSaveButton(event) {
+  event.preventDefault();
+  var entryObject = {};
+  entryObject.entryId = data.nextEntryId;
+  entryObject.photoUrl = $form.elements.photoUrl.value;
+  entryObject.title = $form.elements.title.value;
+  entryObject.notes = $form.elements.notes.value;
+  data.nextEntryId++;
+  data.entries.unshift(entryObject);
+  $photoUrlInput.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
+}
+
+$saveButton.addEventListener('submit', clickSaveButton);
